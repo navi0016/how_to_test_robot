0a2a0cc03a4c58b9d112ab4e0eee1a4b208c11bb
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-15 13:56:10 -0400

    finished

--- test_robot.rb
+++ test_robot.rb
@@ -1,3 +1,4 @@
+gem 'activesupport', '5.1.3'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './robot.rb'
@@ -5,75 +6,92 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
+
     # arrange
-
+      a = Robot.new
+      a.needs_repairs = true
+      a.foreign_model = true
     # act
-
+     result = a.station
     # assert
+    assert_equal(1,result)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
+
     # arrange
-
+    a = Robot.new
+    a.needs_repairs = true
+    a.vintage_model = true
     # act
-
+    result = a.station
     # assert
+    assert_equal(2,result)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    a = Robot.new
+    a.needs_repairs = true
     # act
-
+    result = a.station
     # assert
+    assert_equal(3,result)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
+
     # arrange
-
+    a = Robot.new
     # act
-
+    result = a.station
     # assert
+    assert_equal(4,result)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
+
     # arrange
+      a = Robot.new
 
     # act
-
+    result = a.prioritize_tasks
     # assert
+    assert_equal(-1,result)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
+
     # arrange
-
+    a = Robot.new
+    a.todos = [1,2,3,4,5]
     # act
-
+      result = a.prioritize_tasks
     # assert
+    assert_equal(5,result)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
+
     # arrange
+    a = Robot.new
+    a.day_off = "Sunday"
 
     # act
-
+    result = a.workday?("Sunday")
     # assert
+    assert_equal(false,result)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
+
     # arrange
-
+    a = Robot.new
+    a.day_off = "Saturday"
     # act
-
+    result = a.workday?("Monday")
     # assert
+    assert_equal(true,result)
   end
 
 end
